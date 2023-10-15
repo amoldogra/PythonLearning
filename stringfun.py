d269e9249d08e6c33e4078d4b86f33da7d2f94c0
Amol Dogra <82282965+amoldogra@users.noreply.github.com>
2023-10-15 11:23:40 +0530

    Add files via upload
    String Function

---
+++ stringfun.py
@@ -0,0 +1,14 @@
+w = "Welcome"
+print(w.find('e'))
+print(w.find('e',3))  # 3 is used to start find from index 3
+print(w.index('l'))
+print(w.index('e',3))
+print(w.isalpha())  # to check all elemets are alphabets
+a = "123"
+print(a.isdigit())  # to check all elemets are digits
+b = "Welcome123"
+print(b.isalnum()) # to check variable contain alphabets and number without space & if any special character find in this will give output as FALSE
+
+print(chr(65))   # to convert numeric value to ASCII value
+print(ord("A"))   # to convert ASCII value to numeric value
+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
