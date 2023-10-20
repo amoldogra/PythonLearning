9292e220afba7a09bf4d05c371dfd20e26d0b7f6
amoldogra <amol88658@gmail.com>
2023-10-20 10:31:03 +0530

    dictionary added

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+d = {
+    "name" : "AMOL",
+    "language" : "python",
+    "time" : "2hrs."
+}
+print(d)
+print(type(d))
+
+print(d["time"])
+print(d["language"])
+print(d["name"])
+
+for n in d:
+    print(n)
+    print(d[n])
