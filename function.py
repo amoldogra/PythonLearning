a16eed621a971e202afd0f52f07619693e1ef39d
amoldogra <amol88658@gmail.com>
2023-10-21 09:40:19 +0530

    functions added

---
+++ function.py
@@ -0,0 +1,17 @@
+# SIMPLE FUNCTION
+def greeting():
+    print("WELCOME TO PYTHON WORLD")
+
+greeting()    
+
+# FUNCTION WITH ARGUMENTS
+def addition(a,b):
+    print(a+b)
+
+addition(10,20)    
+
+#FUNCTION WITH RETURN STATEMENT
+def multiply(a,b):
+    return a*b
+
+print(multiply(2,4))
