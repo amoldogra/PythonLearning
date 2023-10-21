7b5a35e05a8d97cf132d6a42a9548e2bd2b146a8
amoldogra <amol88658@gmail.com>
2023-10-21 07:51:37 +0530

    tuple added

---
+++ tuple.py
@@ -0,0 +1,24 @@
+# IMMUTBALE - NON-CHNAGABLE
+# CONTAIN MORE THAN ONE ELEMENT
+
+t = (10,20,30,40,20)
+l = len(t)
+for a in range(l):
+    print(a)
+for b in t:
+    print(b)
+
+#min func.
+print(min(t))
+
+#max func.
+print(max(t))
+
+#count func.
+print(t.count(20))
+
+#index func.
+print(t.index(20,2,5))
+
+#sum func.
+print(sum(t))               #addition of all elements of tupple
