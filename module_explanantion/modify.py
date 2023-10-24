cab674faab609cbcc892abf9ea94f51277e4e510
amoldogra <amol88658@gmail.com>
2023-10-24 10:34:25 +0530

    dir. name changed

---
+++ module_explanantion/modify.py
@@ -0,0 +1,4 @@
+def sum(a,b):
+    print(a+b)
+def mul(a,b):
+    print(a*b)    
---
+++ module_explanantion/module1.py
@@ -0,0 +1,7 @@
+# FIRST WAY TO IMPORT FUNCTION FROM DIFFERENT MODULE OR .py FILE
+import modify
+
+modify.sum(4,6)
+modify .mul(2,10)
+
+
