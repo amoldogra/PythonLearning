f61f996acbe4b7952b1a29faa871fdaafbf6c26f
amoldogra <amol88658@gmail.com>
2023-10-24 10:58:43 +0530

    mathfunction  added

---
+++ mathmodules.py
@@ -0,0 +1,23 @@
+import math
+
+#ceil func. - to convert decimal value into next digit to it
+x = 11.2
+print(math.ceil(x))
+
+#fabs func. - return absolute value of variable always give +ve value
+z = -10
+print(math.fabs(z))
+
+#fractorial func. - return factorial of variable value
+p = 5
+print(math.factorial(p))
+
+#floor func. - return lower value of any decimal variable
+print(math.floor(x))
+
+#fsum func -  addition of all elements of list and gives ans. in float
+l = [10,20,30,40]
+print(math.fsum(l))
+
+#sqrt func.- return square root
+print(math.sqrt(16))
