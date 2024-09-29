09c3b0af3d10a6aefe5f8b9156c308e1a0e86a73
Amol Dogra <amol88658@gmail.com>
2024-09-29 21:34:32 +0530

    code added

---
+++ PYTHON_PROBLEMS/file_io_problems/3.py
@@ -0,0 +1,12 @@
+#tables generated in mentioned folder using file handling 
+# after run this code folder is filled with the desired tables 
+def generatetable(n):
+    table=""
+    for i in range (1,11):
+        table += f"{n} X {i} = {n*i}\n"
+
+    with open (f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+for i in range(2,21):
+    generatetable(i)
