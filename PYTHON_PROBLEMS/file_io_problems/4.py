2ea8bd354d5f16e9faa3c2b19608774828a57847
Amol Dogra <amol88658@gmail.com>
2024-09-29 21:47:20 +0530

    code updated

--- PYTHON_PROBLEMS/file_io_problems/4.py
+++ PYTHON_PROBLEMS/file_io_problems/4.py
@@ -9,3 +9,14 @@ with open("donkey4.txt", "r") as f:
 contentNew = content.replace(word, "####")
 with open("donkey4.txt", "w") as f:
     f.write(contentNew)
+
+
+#  if we need to replace multiple word with single word 
+# words = ["Donkey","Monkey","Ponkey"]
+
+# with open("donkey4.txt", "r") as f:
+#     content = f.read()
+# for word in word:
+#      content = content.replace(word, "#" *len(word))
+# with open("donkey4.txt", "w") as f:
+#     f.write(content)
